01c31c4b2fdead48677d7a5fbbffb28e5212b5d4
Cekepek <ckpk7171@gmail.com>
2023-03-20 13:11:45 +0700

    init + fragment + navigation

---
+++ app/src/main/java/com/cekepek/adv160420021week4/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cekepek.adv160420021week4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
