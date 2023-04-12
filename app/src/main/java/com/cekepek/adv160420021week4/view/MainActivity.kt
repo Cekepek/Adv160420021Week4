4293cfa4cd0d6159996eabc03bcb5f2c5bde590e
Cekepek <ckpk7171@gmail.com>
2023-04-12 10:38:25 +0700

    Week 7 rxjava

--- app/src/main/java/com/cekepek/adv160420021week4/view/MainActivity.kt
+++ app/src/main/java/com/cekepek/adv160420021week4/view/MainActivity.kt
@@ -2,11 +2,42 @@ package com.cekepek.adv160420021week4.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.cekepek.adv160420021week4.R
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val observable = Observable.just("a stream of data", "hello", "world")
+
+        val observer = object : Observer<String> {
+            override fun onSubscribe(d: Disposable?) {
+                Log.d("Messages", "begin subscribe")
+            }
+
+            override fun onNext(t: String?) {
+                Log.d("Messages", "data: $t")
+            }
+
+            override fun onError(e: Throwable?) {
+                Log.e("Messages", "error: ${e!!.message.toString()}")
+            }
+
+            override fun onComplete() {
+                Log.d("Messages", "complete")
+            }
+        }
+        observable
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(observer)
+
     }
 }
