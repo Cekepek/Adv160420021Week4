8a051178f7fbb278fa59175dd72a3fe07115cdb0
Cekepek <ckpk7171@gmail.com>
2023-04-12 11:16:54 +0700

    Week 7 notification

--- app/src/main/java/com/cekepek/adv160420021week4/view/MainActivity.kt
+++ app/src/main/java/com/cekepek/adv160420021week4/view/MainActivity.kt
@@ -3,7 +3,10 @@ package com.cekepek.adv160420021week4.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.cekepek.adv160420021week4.R
+import com.cekepek.adv160420021week4.util.createNotificationChannel
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
@@ -11,33 +14,62 @@ import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val observable = Observable.just("a stream of data", "hello", "world")
-
-        val observer = object : Observer<String> {
-            override fun onSubscribe(d: Disposable?) {
-                Log.d("Messages", "begin subscribe")
-            }
-
-            override fun onNext(t: String?) {
-                Log.d("Messages", "data: $t")
-            }
-
-            override fun onError(e: Throwable?) {
-                Log.e("Messages", "error: ${e!!.message.toString()}")
-            }
-
-            override fun onComplete() {
-                Log.d("Messages", "complete")
-            }
-        }
-        observable
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(observer)
-
+    init {
+        instance = this
     }
-}
+
+    companion object {
+        private var instance: MainActivity? = null
+
+        fun showNotification(title: String, content: String, icon: Int) {
+            val channelId = "${instance?.packageName}-${instance?.getString(R.string.app_name)}"
+            val notificationBuilder =
+                NotificationCompat.Builder(instance!!.applicationContext, channelId).apply {
+                    setSmallIcon(icon)
+                    setContentTitle(title)
+                    setContentText(content)
+                    setStyle(NotificationCompat.BigTextStyle())
+                    priority = NotificationCompat.PRIORITY_DEFAULT
+                    setAutoCancel(true)
+
+                }
+            val notificationManager =
+                NotificationManagerCompat.from(instance!!.applicationContext.applicationContext!!)
+            notificationManager.notify(1001, notificationBuilder.build())
+
+        }
+    }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            val observable = Observable.just("a stream of data", "hello", "world")
+
+            val observer = object : Observer<String> {
+                override fun onSubscribe(d: Disposable?) {
+                    Log.d("Messages", "begin subscribe")
+                }
+
+                override fun onNext(t: String?) {
+                    Log.d("Messages", "data: $t")
+                }
+
+                override fun onError(e: Throwable?) {
+                    Log.e("Messages", "error: ${e!!.message.toString()}")
+                }
+
+                override fun onComplete() {
+                    Log.d("Messages", "complete")
+                }
+            }
+            observable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(observer)
+            createNotificationChannel(this,
+                NotificationManagerCompat.IMPORTANCE_DEFAULT, false,
+                getString(R.string.app_name), "App notification channel.")
+
+        }
+    }
--- app/src/main/java/com/cekepek/adv160420021week4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/cekepek/adv160420021week4/view/StudentDetailFragment.kt
@@ -1,6 +1,7 @@
 package com.cekepek.adv160420021week4.view
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,7 +13,11 @@ import com.cekepek.adv160420021week4.util.loadImage
 import com.cekepek.adv160420021week4.viewmodel.DetailViewModel
 import com.cekepek.adv160420021week4.viewmodel.ListViewModel
 import com.squareup.picasso.Picasso
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_student_detail.*
+import java.util.concurrent.TimeUnit
 
 class StudentDetailFragment : Fragment() {
     private lateinit var viewModel: DetailViewModel
@@ -44,6 +49,18 @@ class StudentDetailFragment : Fragment() {
             txtBod.setText(it.bod)
             txtPhone.setText(it.phone)
             imageView2.loadImage(it.photoUrl,progressBarDetail)
+            var student = it
+            btnNotif.setOnClickListener {
+                Observable.timer(5, TimeUnit.SECONDS)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        Log.d("Messages", "five seconds")
+                        MainActivity.showNotification(student.name.toString(),
+                            "A new notification created",
+                            R.drawable.ic_baseline_error_24)
+                    }
+            }
         })
     }
 }
